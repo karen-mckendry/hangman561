3aa1f4d84670864fa9e3e237e9a736ddb3ae3284
karen.mckendry <karen.mckendry@talktalk.net>
2024-04-26 19:56:49 +0100

    Updates milestone_5.py and README.md

--- README.md
+++ README.md
@@ -17,13 +17,27 @@ This is a version of the Hangman game, where a user has to guess which fruit the
 
 Development of the game started with defining the word list and using the choice method of Python's random module to select one at random. The user is asked for input which is validated to ensure the length = 1 and the input is alphabetical using the isalpha() method. Next, this was put inside a function, and another function was defined to check if the guess was in the word, having converted it to lower case. In the following stage, a class was defined and some attributes initialised: the randomly chosen word, the guessed word, the number of unique letters not yet guessed, how many lives remain, the list of possible words and the list of letters already guessed. The functions were moved inside the class and enhanced to update the guessed word with correctly guessed letters, and to adjust to the number of lives and the list of guessed letters.
 
+## Class and Methods
+
+class Hangman:
+This class creates the game, with the following methods:
+&emsp;&emsp;def check_guess (guess):   
+&emsp;&emsp;&emsp;This method is used to check if the letter input by the user is in the randomly selected word.  
+&emsp;&emsp;def ask_for_input():  
+&emsp;&emsp;&emsp;This method requests input of a letter from the user and validates it, then calls the check_guess function to check if it's in the word.  
+  
+def play_game(word_list):  
+&emsp;This function creates a game of class Hangman and continues until the user guesses all letters, or their lives run out.    
+
 ## Installation
 
-git clone https://github.com/karen-mckendry/hangman561
+In your terminal input: `git clone https://github.com/karen-mckendry/hangman561`
+
+To play the game input: `python milestone_5.py`
 
 ## Usage
 
-Follow the on-screen instructions to guess letters and try to uncover the chosen word.
+Follow the on-screen instructions to guess letters and try to uncover the selected word.
 
 ## License
 
--- milestone_5.py
+++ milestone_5.py
@@ -1,21 +1,67 @@
 import random
 
 class Hangman:
+    '''
+    A class for the Hangman game.
+
+    ...
+    Attributes
+    ----------
+    word : str
+        randomly selected word from word list
+    word_guessed : list
+        list with same length as selected word, representing word with guessed letters so far
+    num_letters: int
+        number of unique letters not yet guessed
+    num_lives: int
+        number of remaining lives
+    list_of_guesses: list
+        list of letters already guessed
+
+    Methods
+    -------
+    def check_guess (guess):
+        Checks if the letter input by the user is in the randomly selected word.
+    '''
 
     def __init__(self, word_list, num_lives=5):
+        '''
+        Sets up the necessary attributes for the game.
+
+        Parameters
+        ----------
+        word : str
+            randomly selected word from word list
+        word_guessed : list
+            list with same length as selected word, representing word with guessed letters so far
+        num_letters: int
+            number of unique letters not yet guessed
+        num_lives: int
+            number of remaining lives
+        list_of_guesses: list
+            list of letters already guessed        
+        '''
+        
         self.word = random.choice(word_list)
         self.word_guessed = ['_']*len(self.word)
-        self.num_letters = len(set(self.word))      # no of unique letters not yet guessed
+        self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.word_list = word_list
-        self.list_of_guesses = []                   # list of letters already guessed
+        self.list_of_guesses = []                   
 
     def check_guess (self, guess):
         '''
-        This method is used to check if the letter input by the user is in the randomly selected word
-        Args:   guess: the letter being checked
-        Returns:    None
+        Checks if the letter input by the user is in the randomly selected word.
+        
+        Parameters
+        ----------
+        guess : the letter being checked
+
+        Returns
+        -------
+        None
         '''
+
         guess = guess.lower()
         if guess in self.word:
             print(f'Good guess! {guess} is in the word.')
@@ -23,19 +69,25 @@ class Hangman:
                 if self.word[i] == guess:
                     self.word_guessed[i] = self.word[i]
             self.num_letters = self.num_letters - 1
-            print(self.word_guessed)
         else:
             self.num_lives = self.num_lives - 1
-            print(f'Sorry, {guess} is not in the word. Try again.')
+            print(f'Sorry, {guess} is not in the word.')
             print(f'You have {self.num_lives} lives left')
+        return
     
     def ask_for_input(self):
         '''
-        This method requests input of a letter from the user and validates it, calling the check_guess function to
-        check if it's in the word
-        Args: None
-        Returns: None
+        Requests input of a letter from the user and validates it, calling the check_guess function to check if it's in the word.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
         '''
+
         while True:
             guess = input('Enter a letter: ')
             if len(guess) !=1 or not guess.isalpha():
@@ -45,14 +97,23 @@ class Hangman:
             else:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess.lower())
-                return
+            print(f'{self.word_guessed} \t\t Letters guessed so far:', ', '.join(str(x) for x in sorted(self.list_of_guesses)))
+            return
     
 def play_game(word_list):
     '''
-    This function creates a game of class Hangman and continues until the user guesses all letters, or their lives run out    
-    Args: word_list
-    Returns: None    
+    Creates a game of class Hangman and continues until the user guesses all letters, or their lives run out.
+
+    Parameters
+    ----------
+    word_list : list
+        list of words from which to randomly select word to be guessed
+    
+    Returns
+    -------
+    None    
     '''
+
     num_lives = 5
     game = Hangman(word_list, num_lives)
     while True:
@@ -65,10 +126,6 @@ def play_game(word_list):
             print('Congratulations. You won the game!')
             return
 
-
-
 word_list = ['strawberry','blueberry', 'cherry', 'peach', 'grape']
 
 play_game(word_list)
-
-
